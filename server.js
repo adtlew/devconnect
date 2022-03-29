ac072abbc18e5fb7544ef75c6e1e1d3d660d6017
Lewainne Vidal <lewainne.vidal@adtelligent.net>
2022-03-29 16:17:16 -0500

    User model added in

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require ('express');
 const app = express();
 const connectDB = require('./config/db');
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
@@ -11,6 +12,14 @@ app.listen(PORT, () => {
 //Testing Database Connection
 connectDB();
 
+
+
+
 app.get('/', (request, response) => {
     response.send('Dev Connect has started...');
 });
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
