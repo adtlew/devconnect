17c51949d078ec1233b624cc1f7428e62b2f9959
Lewainne Vidal <lewainne.vidal@adtelligent.net>
2022-03-29 14:31:20 -0500

    Establishing database connection..

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require ('express');
 const app = express();
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
-const mongoose = require ('mongoose');
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
+
+//Testing Database Connection
+connectDB();
 
 app.get('/', (request, response) => {
     response.send('Dev Connect has started...');
